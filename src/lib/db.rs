d8be9f4a9a263c41971e7149c3add5446fb5c43c
Mike Valstar <mvalstar@konradgroup.com>
2025-01-03 09:14:10 -0500

    updated names to match new package name

--- src/lib/db.rs
+++ src/lib/db.rs
@@ -24,8 +24,8 @@ pub fn list_bullets(date: &str) -> Result<Vec<BItem>> {
     let db_path = database_location();
     let conn = Connection::open(db_path)?;
 
-    let mut stmt =
-        conn.prepare("SELECT id, quickid, text, list_date, added FROM blist WHERE list_date = ?1")?;
+    let mut stmt = conn
+        .prepare("SELECT id, quickid, text, list_date, added FROM bjourn WHERE list_date = ?1")?;
     let bullet_iter = stmt.query_map(params![date], |row| {
         Ok(BItem {
             id: row.get(0)?,
@@ -55,7 +55,7 @@ pub fn add_bullet(text: &String) -> Result<()> {
     let quickid = nanoid!(8, &ALPHABET);
 
     conn.execute(
-        "INSERT INTO blist (quickid, added, list_date, text) VALUES (?1, ?2, ?3, ?4)",
+        "INSERT INTO bjourn (quickid, added, list_date, text) VALUES (?1, ?2, ?3, ?4)",
         params![quickid, now_str, date_str, text],
     )?;
 
@@ -67,7 +67,7 @@ pub fn remove_bullet(quickid: &String) -> Result<()> {
     let db_path = database_location();
     let conn = Connection::open(db_path)?;
 
-    conn.execute("DELETE FROM blist WHERE quickid = ?1", params![quickid])?;
+    conn.execute("DELETE FROM bjourn WHERE quickid = ?1", params![quickid])?;
 
     Ok(())
 }
@@ -97,7 +97,7 @@ pub fn create_database() -> Result<()> {
 
     // Create the bullet list
     conn.execute(
-        "CREATE TABLE IF NOT EXISTS blist (
+        "CREATE TABLE IF NOT EXISTS bjourn (
           id INTEGER PRIMARY KEY AUTOINCREMENT,
           quickid TEXT NOT NULL,
           added TEXT NOT NULL,
@@ -113,17 +113,17 @@ pub fn create_database() -> Result<()> {
 pub fn database_location() -> String {
     let db_path: String;
 
-    if std::env::var("BLIST_DB").is_ok() {
-        db_path = std::env::var("BLIST_DB").unwrap();
+    if std::env::var("BJOURN_DB").is_ok() {
+        db_path = std::env::var("BJOURN_DB").unwrap();
     } else {
         let db_dir = my_home().unwrap();
         match db_dir {
             Some(mut dir) => {
-                dir.push(".blist.db");
+                dir.push(".bjourn.db");
                 db_path = dir.into_os_string().into_string().unwrap();
             }
             None => {
-                println!("Could not find home directory or BLIST_DB environment variable");
+                println!("Could not find home directory or BJOURN_DB environment variable");
                 std::process::exit(1);
             }
         }
